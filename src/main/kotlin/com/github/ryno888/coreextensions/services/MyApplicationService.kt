54e7a37c98bb2a258837629a4be8baa37f493dea
GitHub Action <action@github.com>
2022-03-03 08:02:33 +0000

    Template cleanup

--- src/main/kotlin/com/github/ryno888/coreextensions/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/ryno888/coreextensions/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.ryno888.coreextensions.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.ryno888.coreextensions.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/ryno888/coreextensions/services/MyProjectService.kt
+++ src/main/kotlin/com/github/ryno888/coreextensions/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.ryno888.coreextensions.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.ryno888.coreextensions.MyBundle
 
 class MyProjectService(project: Project) {
 
